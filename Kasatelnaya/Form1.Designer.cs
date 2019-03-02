3de3c116a2a4391db7cba750659cb066ecaf80bf
Yanholly <m9khamets@gmail.com>
2019-03-02 12:45:09 +0300

    First version

---
+++ Kasatelnaya/Form1.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace Kasatelnaya
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown4 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown5 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(762, 481);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(878, 47);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(85, 20);
+            this.numericUpDown1.TabIndex = 2;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(878, 73);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(85, 20);
+            this.numericUpDown2.TabIndex = 3;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            550,
+            0,
+            0,
+            0});
+            this.numericUpDown2.ValueChanged += new System.EventHandler(this.numericUpDown2_ValueChanged);
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(878, 99);
+            this.numericUpDown3.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(85, 20);
+            this.numericUpDown3.TabIndex = 4;
+            this.numericUpDown3.Value = new decimal(new int[] {
+            250,
+            0,
+            0,
+            0});
+            this.numericUpDown3.ValueChanged += new System.EventHandler(this.numericUpDown3_ValueChanged);
+            // 
+            // numericUpDown4
+            // 
+            this.numericUpDown4.Increment = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numericUpDown4.Location = new System.Drawing.Point(878, 142);
+            this.numericUpDown4.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDown4.Name = "numericUpDown4";
+            this.numericUpDown4.Size = new System.Drawing.Size(85, 20);
+            this.numericUpDown4.TabIndex = 6;
+            this.numericUpDown4.Value = new decimal(new int[] {
+            104,
+            0,
+            0,
+            0});
+            this.numericUpDown4.ValueChanged += new System.EventHandler(this.numericUpDown4_ValueChanged);
+            // 
+            // numericUpDown5
+            // 
+            this.numericUpDown5.Increment = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numericUpDown5.Location = new System.Drawing.Point(878, 168);
+            this.numericUpDown5.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDown5.Name = "numericUpDown5";
+            this.numericUpDown5.Size = new System.Drawing.Size(85, 20);
+            this.numericUpDown5.TabIndex = 5;
+            this.numericUpDown5.Value = new decimal(new int[] {
+            104,
+            0,
+            0,
+            0});
+            this.numericUpDown5.ValueChanged += new System.EventHandler(this.numericUpDown5_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(780, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Радиус";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(780, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "х круга";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(780, 101);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "y круга";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(780, 144);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "x точки";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(780, 170);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(43, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "у точки";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(961, 505);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown4);
+            this.Controls.Add(this.numericUpDown5);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown5)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.NumericUpDown numericUpDown4;
+        private System.Windows.Forms.NumericUpDown numericUpDown5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Kasatelnaya/Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kasatelnaya
+{
+    public partial class Form1 : Form
+    {
+        bool isCircClicked = false;
+        bool isPointClicked = false;
+        int dX = 0;
+        int dY = 0;
+        int circx = 550 ,circy = 250, pointx = 104, pointy = 104, rad = 100;
+
+        Point peres = new Point();
+  
+        public Form1()
+        {
+
+            InitializeComponent();
+             
+        }
+
+     
+
+        private void FindPeres()
+        {
+            int Dx = Math.Abs(circx-pointx);
+            int Dy = Math.Abs(pointy - circy);
+            int L = (int)Math.Sqrt(Math.Pow(Math.Abs(circx - pointx), 2) + Math.Pow(Math.Abs(pointy - circy), 2));
+            int le = L - rad;
+            int dxe = (Dx * le) / L;
+            int dye = (Dy * le) / L;
+            if (circx > pointx)
+                peres.X = dxe + pointx;
+            else
+                peres.X = pointx - dxe;
+            if (circy > pointy)
+                peres.Y = dye + pointy;
+            else
+                peres.Y = pointy - dye;
+        }
+
+        private float FindX(int sign)
+        {
+            FindPeres();
+            float L = (float)Math.Sqrt(Math.Pow(Math.Abs(circx - pointx), 2) + Math.Pow(Math.Abs(pointy - circy), 2));
+            float X = circx + (peres.X - circx) * (rad / L) - (peres.Y - circy) * sign * (float)Math.Sqrt(1 - Math.Pow(rad / L, 2));
+            return X;
+        }
+
+        private float FindY(int sign)
+        {
+            FindPeres();
+            float L = (float)Math.Sqrt(Math.Pow(Math.Abs(circx - pointx), 2) + Math.Pow(Math.Abs(pointy - circy), 2));
+            float Y = circy + (peres.X - circx) * sign * (float)Math.Sqrt(1 - Math.Pow(rad / L, 2)) + (peres.Y - circy) * (rad / L);
+            return Y;
+        }
+
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            rad = (int)numericUpDown1.Value;
+            pictureBox1.Invalidate();
+        }
+
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+            circx = (int)numericUpDown2.Value;
+            pictureBox1.Invalidate();
+        }
+
+        private void numericUpDown3_ValueChanged(object sender, EventArgs e)
+        {
+            circy = (int)numericUpDown3.Value;
+            pictureBox1.Invalidate();
+        }
+
+        private void numericUpDown4_ValueChanged(object sender, EventArgs e)
+        {
+            pointx = (int)numericUpDown4.Value;
+            pictureBox1.Invalidate();
+        }
+
+        private void numericUpDown5_ValueChanged(object sender, EventArgs e)
+        {
+            pointy = (int)numericUpDown5.Value;
+            pictureBox1.Invalidate();
+        }
+
+      
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            FindPeres();
+            Pen Circpen = new Pen(Color.Blue, 2);
+            Pen Linecpen = new Pen(Color.Gray, 3);
+            SolidBrush brush = new SolidBrush(Color.Red);
+            e.Graphics.DrawEllipse(Circpen, circx - rad, circy - rad, rad * 2, rad * 2);
+      
+            e.Graphics.DrawLine(Linecpen, pointx, pointy, peres.X, peres.Y);
+
+            e.Graphics.DrawLine(Linecpen, pointx, pointy, FindX(1), FindY(1));
+            e.Graphics.DrawLine(Linecpen, pointx, pointy, FindX(-1), FindY(-1));
+
+            e.Graphics.FillEllipse(brush, pointx - 4, pointy - 4, 8, 8);
+            e.Graphics.FillEllipse(brush, peres.X - 4, peres.Y - 4, 8, 8);
+            e.Graphics.FillEllipse(brush, FindX(1) - 4, FindY(1) - 4, 8, 8);
+            e.Graphics.FillEllipse(brush, FindX(-1) - 4, FindY(-1) - 4, 8, 8);
+
+           
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+
+            if (Math.Pow((e.X - pointx), 2) + Math.Pow((e.Y - pointy), 2) <= Math.Pow(4, 2))
+            {
+                isPointClicked = true;
+                dX = e.X - pointx;
+                dY = e.Y - pointy;
+            }
+
+            if (Math.Pow((e.X - circx), 2) + Math.Pow((e.Y - circy), 2) <= Math.Pow(rad, 2))
+            {
+                isCircClicked = true;
+                dX = e.X - circx;
+                dY = e.Y - circy;
+            }
+
+            
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            isCircClicked = false;
+            isPointClicked = false;
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isPointClicked)
+            {
+                pointx = e.X - dX;
+                pointy = e.Y - dY;
+                pictureBox1.Invalidate();
+            }
+
+            if (isCircClicked)
+            {
+                circx = e.X - dX;
+                circy = e.Y - dY;
+                pictureBox1.Invalidate();
+            }
+           
+        }
+    }
+}
